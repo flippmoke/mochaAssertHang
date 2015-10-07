3af4d5b10d0f149b128ba9747b627b0a2f1e0acb
Blake Thompson <flippmoke@gmail.com>
2015-10-06 17:52:41 -0700

    Updated index to show it is only in mocha

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("DO NOTHING!");
+var assert = require('assert');
+var fs = require('fs');
+
+var a = JSON.parse(fs.readFileSync('c.json'));
+var b = JSON.parse(fs.readFileSync('d.json'));
+
+try {
+    assert.deepEqual(a,b);
+} catch (e) {
+    console.log(e);
+}
